5cbdf9c00295510fe80efe79186a7c4956bad207
Miguel Moya <100721490+Mikemece@users.noreply.github.com>
2022-04-28 11:07:08 +0200

    Delete practicaSensores directory

--- practicaSensores/src/main/java/practicaMockito/practicaSensores/SensorExcepcion.java
+++
@@ -1,9 +0,0 @@
-package practicaMockito.practicaSensores;
-
-@SuppressWarnings("serial")
-public class SensorExcepcion extends RuntimeException {
-
-  public SensorExcepcion(String mensaje) {
-    super(mensaje);
-  }
-}
